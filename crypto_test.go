a11a9a54e524a3e0c9b54cc92c54ab7e71175e30
James Fargher <proglottis@gmail.com>
2014-07-23 19:18:33 +1200

    First test!

---
+++ crypto_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"code.google.com/p/go.crypto/openpgp"
+	"testing"
+)
+
+func TestEntityMatchesId(t *testing.T) {
+	entity, _ := openpgp.NewEntity("Bob", "", "bob@example.com", nil)
+	entity.PrivateKey = nil
+	keyid := entity.PrimaryKey.KeyIdString()
+	keyidshort := entity.PrimaryKey.KeyIdShortString()
+
+	if !EntityMatchesId(entity, "bob@example.com") {
+		t.Error("must match by email")
+	}
+	if !EntityMatchesId(entity, keyid) {
+		t.Error("must match by key ID")
+	}
+	if !EntityMatchesId(entity, keyidshort) {
+		t.Error("must match by short key ID")
+	}
+	if EntityMatchesId(entity, "no_match") {
+		t.Error("should not match")
+	}
+}
