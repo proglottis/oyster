c11335ae2a223d81baae4473ca1afbf1d9397c05
James Fargher <proglottis@gmail.com>
2014-08-01 16:21:34 +1200

    Save via webservice

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
 	"net/url"
 	"os"
@@ -42,6 +43,8 @@ func (h keysHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "POST":
 		h.GetKey(w, r)
+	case "PUT":
+		h.PutKey(w, r)
 	default:
 		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 	}
@@ -64,16 +67,34 @@ func (h keysHandler) GetKey(w http.ResponseWriter, r *http.Request) {
 	JSON(w, item{Key: key, Value: value})
 }
 
+func (h keysHandler) PutKey(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+	keyurl, err := url.Parse(r.PostForm.Get("url"))
+	if err != nil {
+		panic(err)
+	}
+	key := keyurl.Host + keyurl.Path
+	plaintext, err := h.repo.Put(key)
+	if err != nil {
+		panic(err)
+	}
+	value := r.PostForm.Get("text")
+	io.WriteString(plaintext, value+"\n")
+	plaintext.Close()
+	JSON(w, item{Key: key, Value: value})
+}
+
 func noCache(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 	w.Header().Set("Cache-Control", "max-age=0, no-store, no-cache")
 	next(w, r)
 }
 
 func JSON(w http.ResponseWriter, v interface{}) {
-	bytes, err := json.Marshal(v)
-	if err != nil {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	encoder := json.NewEncoder(w)
+	if err := encoder.Encode(v); err != nil {
 		panic(err)
 	}
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.Write(bytes)
 }
