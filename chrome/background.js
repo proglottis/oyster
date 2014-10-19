c2a3e02d46448de814b1ada4033e1aee2a6a57e2
James Fargher <proglottis@gmail.com>
2014-10-19 14:42:38 +1300

    Show badge if form is on page

---
+++ chrome/background.js
@@ -0,0 +1,18 @@
+(function () {
+  'use strict';
+
+  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    $.ajax({
+      url: "http://localhost:45566/keys",
+      type: "POST",
+      data: JSON.stringify({url: tab.url}),
+      contentType: "application/json; charset=utf-8"
+    })
+    .done(function(data) {
+      chrome.browserAction.setBadgeText({text: "1", tabId: tabId});
+    })
+    .fail(function() {
+      chrome.browserAction.setBadgeText({text: "Fail", tabId: tabId});
+    });
+  });
+}());
