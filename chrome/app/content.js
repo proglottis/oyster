7d291f99da833b0a754270dbd1f0d8ca95a1aa5b
James Fargher <proglottis@gmail.com>
2014-11-26 22:36:07 +1300

    Strip jquery from content script

--- chrome/app/content.js
+++ chrome/app/content.js
@@ -1,19 +1,37 @@
 (function () {
   'use strict';
 
-  chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (!message.type) {
-      return;
+  function setElementsValueByName(name, value) {
+    var elements = document.getElementsByName(name);
+    for(var i = 0; i < elements.length; i++) {
+      elements[i].value = value;
     }
+  }
+
+  function serializeForms() {
+    var fields = [];
+    for(var i = 0; i < document.forms.length; i++) {
+      var elements = document.forms[i].elements;
+      for(var j = 0; j < elements.length; j++) {
+        var el = elements[j];
+        if(el.clientHeight < 1 || el.clientWidth < 1) {
+          continue;
+        }
+        fields.push({name: el.name, value: el.value});
+      }
+    }
+    return fields;
+  }
+
+  chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     switch(message.type) {
     case "SET_FORM":
       message.data.fields.forEach(function(field) {
-        $("form [name='"+field.name+"']").val(field.value);
+        setElementsValueByName(field.name, field.value);
       });
       break;
     case "GET_FORM":
-      var fields = $(':input:visible').not('[type=hidden]').serializeArray();
-      sendResponse(fields);
+      sendResponse(serializeForms());
       break;
     }
   });
