4a856a344273be778b4dd1503d7d1b8c153ae2c1
James Fargher <proglottis@gmail.com>
2014-12-11 14:43:15 +1300

    Stop appending / to the key when there is no path

--- chrome/app/background.js
+++ chrome/app/background.js
@@ -2,22 +2,34 @@
 
 var Uri = require('jsuri');
 
+function urlKey(url) {
+    var uri = new Uri(url);
+    var key = uri.host();
+    if(uri.path() !== "/") {
+      key += uri.path();
+    }
+    return key;
+}
+
+function newFormPopup(tabId, url) {
+  chrome.tabs.sendMessage(tabId, {type: "GET_FORM"}, function(fields) {
+    var form = {
+      tabId: tabId,
+      key: urlKey(url),
+      fields: fields
+    };
+    chrome.windows.create({url: 'newform.html', type: 'popup', width: 400, height: 500}, function(){
+      chrome.runtime.sendMessage(form);
+    });
+  });
+}
+
 chrome.runtime.onInstalled.addListener(function() {
   chrome.contextMenus.create({
     title: 'Save Page Fields',
     contexts: ['all'],
     onclick: function(info, tab) {
-      chrome.tabs.sendMessage(tab.id, {type: "GET_FORM"}, function(fields) {
-        var uri = new Uri(tab.url);
-        var form = {
-          tabId: tab.id,
-          key: uri.host() + uri.path(),
-          fields: fields
-        };
-        chrome.windows.create({url: 'newform.html', type: 'popup', width: 400, height: 500}, function(){
-          chrome.runtime.sendMessage(form);
-        });
-      });
+      newFormPopup(tab.id, tab.url);
     }
   });
 });
